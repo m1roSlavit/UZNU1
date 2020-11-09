9a05f831c1aa8dffe55ab53ba44363de0d98e932
m1roSlavit <m1roslavsavit@gmail.com>
2020-11-09 12:38:13 +0200

    change lab 5 ex 3

--- labs/lab5/dest/3.py
+++ labs/lab5/dest/3.py
@@ -11,11 +11,11 @@ equation_right_part = x_value*(1-static_value)*(1-static_value/4)
 n_counter = 6
 
 while True:
-    temp_val = equation_right_part * (1 - static_value/((n_counter - 1)**2))
+    temp_val = 1 - static_value/((n_counter - 1)**2)
     n_counter += 1
     if fabs(temp_val) < E_val:
         break
-    equation_right_part = temp_val
+    equation_right_part *= temp_val
 
 if equation_left_part == equation_right_part:
     print("Рівність справедлива")
